2439eca5be87a8abd901ab7f4fa9ba1ad4b44f40
zavvdev <zaviryukha.igor99@gmail.com>
2024-10-04 22:41:23 +0300

    Step 1: Setup project

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+// allows to direct http request to the correct controller
+app.MapControllers();
+
+app.Run();
